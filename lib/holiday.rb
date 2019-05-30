453bf3df48640f1964476515f19e6974f3636a70
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-30 13:52:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,20 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
 
-     
+     holiday_hash.map do |season, holidays|
+
+    holidays.map do |holiday, items|
+
+      if items.include?("BBQ")
+
+        holiday
+
+      end
+
+    end
+
+  end
+  .flatten.compact
 end
 
 
