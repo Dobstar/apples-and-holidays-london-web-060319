81185b65403a2ef435d3dcaa584f1f5f6c7869c0
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-30 13:51:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,12 +70,15 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |season, holiday|
 
+     
 end
 
 
