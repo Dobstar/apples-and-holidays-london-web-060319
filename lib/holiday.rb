c055af94e9e8dc579eb5344346a79c903521799d
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-30 13:51:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,17 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
 
+      puts season.to_s.capitalize! << ":"
+
+      holiday.each do |holiday, supplies|
+
+        supplies = supplies.join(", ")
+
+        holiday = holiday.to_s.split("_").map {|x| x.capitalize}.join(" ")<< ":"
+
+        puts "  " + holiday + " " + supplies
 end
 
 def all_holidays_with_bbq(holiday_hash)
